10f0887f298724644beb29dc34397b289c7fccd3
omteja04 <22P31A0539@acet.ac.in>
2024-03-03 16:23:51 +0530

    ArrayListInJava

---
+++ OOP/ArrayListInJava.java
@@ -0,0 +1,14 @@
+package OOP;
+
+import java.util.ArrayList;
+
+public class ArrayListInJava {
+    public static void main(String[] args) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        arr.add(10);
+        arr.add(20);
+        System.out.println(arr);
+        System.out.println(arr.get(0));
+        System.out.println(arr.get(1));
+    }
+}
