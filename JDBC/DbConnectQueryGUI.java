8bfdb593919d436c7c57b84910142e3074a111da
omteja04 <22P31A0539@acet.ac.in>
2024-03-23 13:14:05 +0530

    Insert using GUI

---
+++ JDBC/DbConnectQueryGUI.java
@@ -0,0 +1,43 @@
+package JDBC;
+
+import java.sql.*;
+import java.util.Scanner;
+
+import javax.swing.JOptionPane;
+
+public class DbConnectQueryGUI {
+    public static void main(String[] args) {
+        try {
+            String url = "jdbc:mysql://localhost:3306/jdbc_temp";
+            String username = "root";
+            String password = "teja1234";
+
+            Connection con = DriverManager.getConnection(url, username, password);
+
+            if (con != null) {
+                System.out.println("Successfully connected");
+            }
+
+            Statement st = con.createStatement();
+
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Enter the no of insertions: ");
+            int n = sc.nextInt();
+            for (int i = 1; i <= n; i++) {
+                String id = JOptionPane.showInputDialog(null, "Enter id of Employee");
+                String name = JOptionPane.showInputDialog(null, "Enter Name of Employee " + ":");
+                String salary = JOptionPane.showInputDialog(null, "Enter Salary of Employee " + ":");
+
+                String query = "INSERT INTO employee VALUES (" + id + ",'" + name + "'," + salary + ")";
+                st.executeUpdate(query);
+            }
+
+            sc.close();
+            System.out.println("Data inserted successfully!");
+
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+
+    }
+}
