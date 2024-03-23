b0640f8094e62ce09cf09b0d97d2154942db7e5f
omteja04 <22P31A0539@acet.ac.in>
2024-03-23 13:13:25 +0530

    JDBC Insert

---
+++ JDBC/DbConnectQueries.java
@@ -0,0 +1,28 @@
+package JDBC;
+
+import java.sql.*;
+
+public class DbConnectQueries {
+    public static void main(String[] args) {
+        try {
+            String url = "jdbc:mysql://localhost:3306/jdbc_temp";
+            String username = "root";
+            String password = "teja1234";
+
+            Connection con = DriverManager.getConnection(url, username, password);
+
+            if (con != null) {
+                System.out.println("Successfully connected");
+            }
+
+            Statement st = con.createStatement();
+            st.executeUpdate("insert into employee values(4,'Warren Thornton','586443')");
+
+
+            System.out.println();
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+
+    }
+}
