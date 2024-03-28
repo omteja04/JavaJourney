235b288c6a495ebf94ee5e43df736277cc57754a
omteja04 <22P31A0539@acet.ac.in>
2024-03-28 23:18:03 +0530

    Multiple Frames Trigger

---
+++ GUIFundamentals/Trigger.java
@@ -0,0 +1,12 @@
+/**
+ * Author: omteja04
+ * Description: Trigger
+ */
+
+package GUIFundamentals;
+
+public class Trigger {
+    public static void main(String[] args) {
+        new MainFrame();
+    }
+}
