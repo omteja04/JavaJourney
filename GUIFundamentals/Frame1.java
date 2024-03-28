915b4539857cc38c6f6c0676b13b10a9dd9b0008
omteja04 <22P31A0539@acet.ac.in>
2024-03-28 23:16:31 +0530

    Multiple Frames - 1

---
+++ GUIFundamentals/Frame1.java
@@ -0,0 +1,19 @@
+/**
+ * Author: omteja04
+ * Description: Frame1
+ */
+
+package GUIFundamentals;
+
+import javax.swing.JFrame;
+
+public class Frame1 extends JFrame {
+    Frame1() {
+        setTitle("Frame 1");
+        setSize(300, 300);
+        setLayout(null);
+        
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        setVisible(true);
+    }
+}
