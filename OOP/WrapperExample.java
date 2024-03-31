d1693b8e97f474d0d3ce9a73c8adfc655951536e
omteja04 <22P31A0539@acet.ac.in>
2024-03-31 13:45:54 +0530

    WrapperExample

--- OOP/WrapperExample.java
+++ OOP/WrapperExample.java
@@ -35,7 +35,6 @@ public class WrapperExample {
             System.out.print(i + " ");
         }
     }
-
     public static void main(String[] args) {
         int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
         printArray(arr);
