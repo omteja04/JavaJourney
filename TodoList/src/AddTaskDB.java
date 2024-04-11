ee538db0b4d3dc65bb51a6e933f6e7bace02832b
omteja04 <22P31A0539@acet.ac.in>
2024-04-11 16:13:19 +0530

    AddTaskDB

---
+++ TodoList/src/AddTaskDB.java
@@ -0,0 +1,53 @@
+/**
+ * Author: omteja04
+ * Description: AddTaskDB
+ */
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import javax.swing.JOptionPane;
+
+public class AddTaskDB {
+
+    public static void addToDatabase(ArrayList<String> valueList) {
+        Connection connection = DatabaseConnection.getConnection();
+
+        if (connection == null) {
+            JOptionPane.showMessageDialog(null, "Database connection is null");
+            return;
+        }
+        String[] columnNames = DatabaseConnection.getColumnNames(connection, "tasks");
+        StringBuilder query = new StringBuilder("INSERT INTO ");
+        query.append("tasks ").append(" (");
+        for (String columnName : columnNames) {
+            query.append(columnName).append(", ");
+        }
+        query.delete(query.length() - 2, query.length()); // Remove the last comma and space
+        query.append(") VALUES (");
+        for (int i = 0; i < columnNames.length; i++) {
+            query.append("?, ");
+        }
+        query.deleteCharAt(query.length() - 2); // Remove the last comma and space
+        query.append(");");
+        try (PreparedStatement statement = connection.prepareStatement(query.toString())) {
+            for (int i = 0; i < valueList.size(); i++) {
+                statement.setString(i + 1, valueList.get(i));
+            }
+            int rowsInserted = statement.executeUpdate();
+            if (rowsInserted > 0) {
+                JOptionPane.showMessageDialog(null, "Add task successfully");
+            } else {
+                JOptionPane.showMessageDialog(null, "Failed to add task");
+            }
+        } catch (SQLException e) {
+
+            e.printStackTrace();
+            JOptionPane.showMessageDialog(null, "Error occurred " + e.getMessage());
+        }
+
+    }
+
+}
