de4732edcbd1a50f8ec64e37226523da7da09c1a
omteja04 <22P31A0539@acet.ac.in>
2024-04-13 00:31:10 +0530

    MergeSort

--- LAB/SortsSearchStrings/MergeSort.java
+++ LAB/SortsSearchStrings/MergeSort.java
@@ -1,29 +1,71 @@
+/**
+ * Author: omteja04
+ * Description: MergeSort
+ */
+
 package LAB.SortsSearchStrings;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
-public class MergeSort {
-    public static void main(String[] args) {
+class MergeSort {
+
+    public static void merge(int arr[], int left, int mid, int right) {
+        int i = left;
+        int j = mid + 1;
+        int k = 0;
+        int temp[] = new int[right - left + 1];
+
+        while (i <= mid && j <= right) {
+            if (arr[i] <= arr[j])
+                temp[k++] = arr[i++];
+            else
+                temp[k++] = arr[j++];
+        }
+
+        while (i <= mid)
+            temp[k++] = arr[i++];
+
+        while (j <= right)
+            temp[k++] = arr[j++];
+
+        k = 0;
+        for (int s = left; s <= right; s++)
+            arr[s] = temp[k++];
+    }
+
+    public static void mergeSort(int arr[], int left, int right) {
+        if (left < right) {
+            int mid = (left + right) / 2;
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid + 1, right);
+            merge(arr, left, mid, right);
+        }
+    }
+
+    public static void printArray(int arr[], int size) {
+        for (int i = 0; i < size; i++)
+            System.out.print(arr[i] + " ");
+    }
+
+    public static void main(String args[]) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter the number of elements in the list:");
+        System.out.println("Enter the number of elements in the array:");
         int n = scanner.nextInt();
 
-        int[] arr = new int[n];
-        System.out.println("Enter the elements to sort:");
-
+        int arr[] = new int[n];
+        System.out.println("Enter the elements of the array:");
         for (int i = 0; i < n; i++) {
             arr[i] = scanner.nextInt();
         }
 
-        // Merge sort
-        Arrays.sort(arr);
+        System.out.println("Given array is");
+        printArray(arr, arr.length);
 
-        System.out.println("Sorted list using Merge Sort:");
-        for (int i : arr) {
-            System.out.print(i + " ");
-        }
+        mergeSort(arr, 0, arr.length - 1);
+
+        System.out.println("\nSorted array is");
+        printArray(arr, arr.length);
 
         scanner.close();
     }
