af783a981f8519201da4ef56c962eeef2159ec49
omteja04 <22P31A0539@acet.ac.in>
2024-04-13 00:16:26 +0530

    Lab

--- LAB/Basics/QuadraticEquationRoots.java
+++ LAB/Basics/QuadraticEquationRoots.java
@@ -1,8 +1,10 @@
-package LAB.Basics;
+
 /**
  * Author: omteja04
  * Description: QuadraticEquationRoots
  */
+
+package LAB.Basics;
 
 import java.util.Scanner;
 
@@ -23,12 +25,17 @@ public class QuadraticEquationRoots {
         if (discriminant > 0) {
             double root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
             double root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
-            System.out.println("Roots are real and different. Root1 = " + root1 + ", Root2 = " + root2);
+            System.out.println("Roots are real and different.\nRoot1 = " + root1 + ", Root2 = " + root2);
         } else if (discriminant == 0) {
             double root = -b / (2 * a);
             System.out.println("Roots are real and same. Root = " + root);
         } else {
-            System.out.println("Roots are complex and different.");
+            double realPart = -b / (2 * a);
+            double imaginaryPart = Math.sqrt(-discriminant) / (2 * a);
+            System.out.println("Roots are complex and different. ");
+            System.out.format("Root1 = %.2f + %.2f i , ", realPart, imaginaryPart);
+            System.out.format("Root2 = %.2f - %.2f i", realPart, imaginaryPart);
+
         }
 
         scanner.close();
